46971c2373418d13dfb32cd6a4c83c1737eda087
bschenermanpwd <ben.schenerman.pwd@gmail.com>
2016-12-07 11:54:23 -0500

    Add files via upload

---
+++ init.js
@@ -0,0 +1,75 @@
+<!--
+//
+// init.js [Communications Portal] V0.1
+// 
+// Dev: Ben Schenerman
+// Project Manager: Paul Fugazzotto
+// Philadelphia Water Department - November 2016
+//
+// Summary:
+//
+// Control Flow:
+// 
+
+$(function() {
+
+//---------------------------------------------------------------------------------------------------
+// Everything
+//---------------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------------
+// Navbar
+//---------------------------------------------------------------------------------------------------
+
+//$('.searchbox').one('click', function(){
+//	$(this).val('');
+//});
+//
+//$('.btn').mouseover(function(){
+//	$(this).css('cursor','pointer');
+//	$(this).css('background-color','#001A70');
+//}).mouseout(function(){
+//	$(this).css('cursor','default');
+//	$(this).css('background','none');
+//	$(this).css('color','white');
+//});
+//
+//$('.btn').mouseup(function(){
+//	$(this).css('background','none');
+//	$(this).css('color','white');
+//}).mousedown(function(){
+//	$(this).css('background-color','#C6DAE7');
+//	$(this).css('color','black');
+//});
+
+
+
+//---------------------------------------------------------------------------------------------------
+// Content
+//---------------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------------
+// Overlay
+//---------------------------------------------------------------------------------------------------
+
+////Open the asset overlay
+//$(".asset").click(function(){
+//	console.log($(this))
+//	//$('#asset-details').css('height','100%');
+//	$('#asset-details').fadeTo('fast',1);
+//	$('body').addClass('no-scroll');
+//});
+//
+////close the asset overlay
+//$(".overlay .closebtn").click(function(){
+//	//$('#asset-details').css('height','0');
+//	$('#asset-details').fadeTo('fast',0);
+//	$('body').removeClass('no-scroll');
+//});
+
+
+//any btn or card: translate the block -3y + 1x on mousedown. restore on mouseup
+ 
+
+});
+-->
